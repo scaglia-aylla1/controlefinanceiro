cd9b6ab8c7731002da5aaaf99235ca2590a53aa1
scaglia-aylla1 <aylla@scaglia.com.br>
2025-09-04 18:57:22 -0300

    feat: Ajustes do deploy2

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,26 @@
-FROM eclipse-temurin:21-jre
-WORKDIR /app
-COPY target/controle-financeiro-0.0.1-SNAPSHOT.jar controle-financeiro-v1.0.jar
-EXPOSE 9090
-ENTRYPOINT ["java", "-jar","controle-financeiro-v1.0.jar"]
+FROM openjdk:17.0.1-jdk-oracle as build
+
+WORKDIR /workspace/app
+
+COPY mvnw .
+COPY .mvn .mvn
+COPY pom.xml .
+COPY src src
+
+RUN chmod -R 777 ./mvnw
+
+RUN ./mvnw install -DskipTests
+
+RUN mkdir -p target/dependency && (cd target/dependency; jar -xf ../*.jar)
+
+FROM openjdk:17.0.1-jdk-oracle
+
+VOLUME /tmp
+
+ARG DEPENDENCY=/workspace/app/target/dependency
+
+COPY --from=build ${DEPENDENCY}/BOOT-INF/lib /app/lib
+COPY --from=build ${DEPENDENCY}/META-INF /app/META-INF
+COPY --from=build ${DEPENDENCY}/BOOT-INF/classes /app
+
+ENTRYPOINT ["java","-cp","app:app/lib/*","com.scaglia.controle_financeiro.ControleFinanceiroApplication"]
--- src/main/java/com/scaglia/controle_financeiro/config/SwaggerConfig.java
+++ src/main/java/com/scaglia/controle_financeiro/config/SwaggerConfig.java
@@ -1,7 +1,13 @@
 package com.scaglia.controle_financeiro.config;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
+import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,11 +15,44 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI apiInfo() {
+    OpenAPI apiInfo() {
         return new OpenAPI()
                 .info(new Info()
                         .title("Controle Financeiro API")
                         .description("Documentação da API para o sistema de Controle Financeiro")
-                        .version("1.0.0"));
+                        .version("v1.0")
+                        .license(new License()
+                                .name("Controle Financeiro"))
+                        .contact(new Contact()
+                                .name("Controle Financeiro")
+                                .url("https://github.com/scaglia-aylla1/controlefinanceiro")
+                                .email("aylla@scaglia.com.br")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Github")
+                        .url("https://github.com/scaglia-aylla1"));
+    }
+
+    @Bean
+    OpenApiCustomizer customizerGlobalHeaderOpenApiCustomizer(){
+        return openApi -> {
+            openApi.getPaths().values().forEach(pathItem -> pathItem.readOperations()
+                    .forEach(operation -> {
+                        ApiResponses apiResponses = operation.getResponses();
+
+                        apiResponses.addApiResponse("200", createApiResponse("Sucesso"));
+                        apiResponses.addApiResponse("201", createApiResponse("Objeto persistido"));
+                        apiResponses.addApiResponse("204", createApiResponse("Objeto excluído"));
+                        apiResponses.addApiResponse("400", createApiResponse("Erro na requisição"));
+                        apiResponses.addApiResponse("401", createApiResponse("Acesso não autorizado"));
+                        apiResponses.addApiResponse("403", createApiResponse("Acesso proibido"));
+                        apiResponses.addApiResponse("404", createApiResponse("Objeto não encontrado"));
+                        apiResponses.addApiResponse("500", createApiResponse("Erro na aplicação"));
+                    })
+            );
+        };
+    }
+
+    private ApiResponse createApiResponse(String message) {
+        return new ApiResponse().description(message);
     }
 }
